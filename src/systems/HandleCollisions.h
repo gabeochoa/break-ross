1af81d053199ef274e012b3fb7115b135e5035db
Gabe Ochoa <gabeochoa@meta.com>
2025-11-29 05:12:08 -0500

    be - refactor HandleCollisions to use ECS for_each pattern

--- src/systems/HandleCollisions.h
+++ src/systems/HandleCollisions.h
@@ -63,43 +63,44 @@ void restore_ball_velocity(Transform &ball_transform, vec2 &stored_velocity) {
 }
 } // namespace
 
-struct HandleCollisions : afterhours::System<> {
-  mutable std::vector<afterhours::Entity *> cached_balls;
-  mutable bool balls_cache_valid{false};
+struct HandleCollisions
+    : afterhours::System<Transform, CanDamage,
+                         afterhours::tags::All<ColliderTag::Circle>> {
+  mutable BrickGrid *cached_brick_grid{nullptr};
+  mutable IsShopManager *cached_shop{nullptr};
+  mutable IsPhotoReveal *cached_photo_reveal{nullptr};
 
   virtual void once(float) override {
-    BrickGrid *brick_grid =
+    cached_brick_grid =
         afterhours::EntityHelper::get_singleton_cmp<BrickGrid>();
-    if (!brick_grid) {
+    if (!cached_brick_grid) {
       log_error("BrickGrid singleton not found");
       return;
     }
 
-    IsShopManager *shop =
-        afterhours::EntityHelper::get_singleton_cmp<IsShopManager>();
-    if (!shop) {
+    cached_shop = afterhours::EntityHelper::get_singleton_cmp<IsShopManager>();
+    if (!cached_shop) {
       log_error("IsShopManager singleton not found");
       return;
     }
 
-    IsPhotoReveal *photo_reveal =
+    cached_photo_reveal =
         afterhours::EntityHelper::get_singleton_cmp<IsPhotoReveal>();
-    if (!photo_reveal) {
+    if (!cached_photo_reveal) {
       log_error("IsPhotoReveal singleton not found");
       return;
     }
+  }
 
-    if (!balls_cache_valid) {
-      cached_balls.clear();
-      auto balls = EQ().whereHasComponent<Transform>()
-                       .whereHasTag(ColliderTag::Circle)
-                       .whereHasComponent<CanDamage>()
-                       .gen();
-      for (afterhours::Entity &ball : balls) {
-        cached_balls.push_back(&ball);
-      }
-      balls_cache_valid = true;
+  virtual void for_each_with(afterhours::Entity &, Transform &ball_transform,
+                             CanDamage &ball_damage, float) override {
+    if (!cached_brick_grid || !cached_shop || !cached_photo_reveal) {
+      return;
     }
+
+    float ball_radius = ball_transform.size.x / 2.0f;
+    vec2 ball_center = {ball_transform.position.x + ball_radius,
+                        ball_transform.position.y + ball_radius};
 
     const float grid_end_x =
         game_constants::BRICK_START_X +
@@ -108,95 +109,83 @@ struct HandleCollisions : afterhours::System<> {
         game_constants::BRICK_START_Y +
         game_constants::GRID_HEIGHT * game_constants::BRICK_CELL_SIZE;
 
-    for (afterhours::Entity *ball_ptr : cached_balls) {
-      afterhours::Entity &ball_entity = *ball_ptr;
-      Transform &ball_transform = ball_entity.get<Transform>();
-      CanDamage &ball_damage = ball_entity.get<CanDamage>();
+    if (ball_center.x + ball_radius < game_constants::BRICK_START_X ||
+        ball_center.x - ball_radius > grid_end_x ||
+        ball_center.y + ball_radius < game_constants::BRICK_START_Y ||
+        ball_center.y - ball_radius > grid_end_y) {
+      return;
+    }
 
-      float ball_radius = ball_transform.size.x / 2.0f;
-      vec2 ball_center = {ball_transform.position.x + ball_radius,
-                          ball_transform.position.y + ball_radius};
+    int min_grid_x =
+        game_constants::world_to_grid_x(ball_center.x - ball_radius);
+    int max_grid_x =
+        game_constants::world_to_grid_x(ball_center.x + ball_radius);
+    int min_grid_y =
+        game_constants::world_to_grid_y(ball_center.y - ball_radius);
+    int max_grid_y =
+        game_constants::world_to_grid_y(ball_center.y + ball_radius);
 
-      if (ball_center.x + ball_radius < game_constants::BRICK_START_X ||
-          ball_center.x - ball_radius > grid_end_x ||
-          ball_center.y + ball_radius < game_constants::BRICK_START_Y ||
-          ball_center.y - ball_radius > grid_end_y) {
-        continue;
-      }
+    if (min_grid_x < 0)
+      min_grid_x = 0;
+    if (max_grid_x >= game_constants::GRID_WIDTH)
+      max_grid_x = game_constants::GRID_WIDTH - 1;
+    if (min_grid_y < 0)
+      min_grid_y = 0;
+    if (max_grid_y >= game_constants::GRID_HEIGHT)
+      max_grid_y = game_constants::GRID_HEIGHT - 1;
 
-      int min_grid_x =
-          game_constants::world_to_grid_x(ball_center.x - ball_radius);
-      int max_grid_x =
-          game_constants::world_to_grid_x(ball_center.x + ball_radius);
-      int min_grid_y =
-          game_constants::world_to_grid_y(ball_center.y - ball_radius);
-      int max_grid_y =
-          game_constants::world_to_grid_y(ball_center.y + ball_radius);
+    bool ball_inside_any_brick = false;
+    vec2 stored_velocity = ball_transform.velocity;
 
-      if (min_grid_x < 0)
-        min_grid_x = 0;
-      if (max_grid_x >= game_constants::GRID_WIDTH)
-        max_grid_x = game_constants::GRID_WIDTH - 1;
-      if (min_grid_y < 0)
-        min_grid_y = 0;
-      if (max_grid_y >= game_constants::GRID_HEIGHT)
-        max_grid_y = game_constants::GRID_HEIGHT - 1;
+    for (int grid_y = min_grid_y; grid_y <= max_grid_y; ++grid_y) {
+      for (int grid_x = min_grid_x; grid_x <= max_grid_x; ++grid_x) {
+        if (!cached_brick_grid->has_brick(grid_x, grid_y)) {
+          continue;
+        }
 
-      bool ball_inside_any_brick = false;
-      vec2 stored_velocity = ball_transform.velocity;
+        float brick_left = game_constants::BRICK_START_X +
+                           grid_x * game_constants::BRICK_CELL_SIZE;
+        float brick_right = brick_left + game_constants::BRICK_CELL_SIZE;
+        float brick_top = game_constants::BRICK_START_Y +
+                          grid_y * game_constants::BRICK_CELL_SIZE;
+        float brick_bottom = brick_top + game_constants::BRICK_CELL_SIZE;
 
-      for (int grid_y = min_grid_y; grid_y <= max_grid_y; ++grid_y) {
-        for (int grid_x = min_grid_x; grid_x <= max_grid_x; ++grid_x) {
-          if (!brick_grid->has_brick(grid_x, grid_y)) {
-            continue;
+        if (ball_center.x + ball_radius < brick_left ||
+            ball_center.x - ball_radius > brick_right ||
+            ball_center.y + ball_radius < brick_top ||
+            ball_center.y - ball_radius > brick_bottom) {
+          continue;
+        }
+
+        if (is_ball_inside_brick_cell(ball_center, brick_left, brick_right,
+                                      brick_top, brick_bottom)) {
+          ball_inside_any_brick = true;
+          handle_ball_inside_brick_cell(ball_transform, stored_velocity);
+          cached_brick_grid->add_health(
+              grid_x, grid_y, static_cast<short>(-ball_damage.amount));
+          if (cached_brick_grid->get_health(grid_x, grid_y) <= 0) {
+            cached_shop->pixels_collected += 1;
+            cached_photo_reveal->set_revealed(grid_x, grid_y);
           }
+          return;
+        }
 
-          vec2 cell_world_pos =
-              game_constants::grid_to_world_pos(grid_x, grid_y);
-          float brick_left = cell_world_pos.x;
-          float brick_right = brick_left + game_constants::BRICK_CELL_SIZE;
-          float brick_top = cell_world_pos.y;
-          float brick_bottom = brick_top + game_constants::BRICK_CELL_SIZE;
-
-          if (ball_center.x + ball_radius < brick_left ||
-              ball_center.x - ball_radius > brick_right ||
-              ball_center.y + ball_radius < brick_top ||
-              ball_center.y - ball_radius > brick_bottom) {
-            continue;
+        if (handle_ball_edge_collision_with_cell(
+                ball_center, ball_radius, ball_transform, brick_left,
+                brick_right, brick_top, brick_bottom)) {
+          cached_brick_grid->add_health(
+              grid_x, grid_y, static_cast<short>(-ball_damage.amount));
+          if (cached_brick_grid->get_health(grid_x, grid_y) <= 0) {
+            cached_shop->pixels_collected += 1;
+            cached_photo_reveal->set_revealed(grid_x, grid_y);
           }
-
-          if (is_ball_inside_brick_cell(ball_center, brick_left, brick_right,
-                                        brick_top, brick_bottom)) {
-            ball_inside_any_brick = true;
-            handle_ball_inside_brick_cell(ball_transform, stored_velocity);
-            brick_grid->add_health(grid_x, grid_y,
-                                   static_cast<short>(-ball_damage.amount));
-            if (brick_grid->get_health(grid_x, grid_y) <= 0) {
-              shop->pixels_collected += 1;
-              photo_reveal->set_revealed(grid_x, grid_y);
-            }
-            goto next_ball;
-          }
-
-          if (handle_ball_edge_collision_with_cell(
-                  ball_center, ball_radius, ball_transform, brick_left,
-                  brick_right, brick_top, brick_bottom)) {
-            brick_grid->add_health(grid_x, grid_y,
-                                   static_cast<short>(-ball_damage.amount));
-            if (brick_grid->get_health(grid_x, grid_y) <= 0) {
-              shop->pixels_collected += 1;
-              photo_reveal->set_revealed(grid_x, grid_y);
-            }
-            goto next_ball;
-          }
+          return;
         }
       }
+    }
 
-      if (!ball_inside_any_brick) {
-        restore_ball_velocity(ball_transform, stored_velocity);
-      }
-
-    next_ball:;
+    if (!ball_inside_any_brick) {
+      restore_ball_velocity(ball_transform, stored_velocity);
     }
   }
 };
